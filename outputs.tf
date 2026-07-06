e2028e1f13f609becbb93174a19aa08ffd25d0ca
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "maintenance_assignment_virtual_machines" {
   description = "All maintenance_assignment_virtual_machine resources"
   value       = azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines
 }
+output "maintenance_assignment_virtual_machines_location" {
+  description = "List of location values across all maintenance_assignment_virtual_machines"
+  value       = [for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : v.location]
+}
+output "maintenance_assignment_virtual_machines_maintenance_configuration_id" {
+  description = "List of maintenance_configuration_id values across all maintenance_assignment_virtual_machines"
+  value       = [for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : v.maintenance_configuration_id]
+}
+output "maintenance_assignment_virtual_machines_virtual_machine_id" {
+  description = "List of virtual_machine_id values across all maintenance_assignment_virtual_machines"
+  value       = [for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : v.virtual_machine_id]
+}
 
