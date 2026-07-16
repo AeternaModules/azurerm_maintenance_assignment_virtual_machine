7c6fb3754a35ffdeab1bd19f2db8144a4e9b767d
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "maintenance_assignment_virtual_machines_id" {
   description = "Map of id values across all maintenance_assignment_virtual_machines, keyed the same as var.maintenance_assignment_virtual_machines"
-  value       = { for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : k => v.id }
+  value       = { for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "maintenance_assignment_virtual_machines_location" {
   description = "Map of location values across all maintenance_assignment_virtual_machines, keyed the same as var.maintenance_assignment_virtual_machines"
-  value       = { for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : k => v.location }
+  value       = { for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "maintenance_assignment_virtual_machines_maintenance_configuration_id" {
   description = "Map of maintenance_configuration_id values across all maintenance_assignment_virtual_machines, keyed the same as var.maintenance_assignment_virtual_machines"
-  value       = { for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : k => v.maintenance_configuration_id }
+  value       = { for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : k => v.maintenance_configuration_id if v.maintenance_configuration_id != null && length(v.maintenance_configuration_id) > 0 }
 }
 output "maintenance_assignment_virtual_machines_virtual_machine_id" {
   description = "Map of virtual_machine_id values across all maintenance_assignment_virtual_machines, keyed the same as var.maintenance_assignment_virtual_machines"
-  value       = { for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : k => v.virtual_machine_id }
+  value       = { for k, v in azurerm_maintenance_assignment_virtual_machine.maintenance_assignment_virtual_machines : k => v.virtual_machine_id if v.virtual_machine_id != null && length(v.virtual_machine_id) > 0 }
 }
 
